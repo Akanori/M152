e17a3de8d22837f280bde40a6e8e58254488c458
Akanori <elias_roth@sluz.ch>
2021-05-20 11:40:21 +0200

    Banus Aufgabe 7-3

--- Uebungen/Uebungen7/Uebung3/uebung3/src/views/About.vue
+++ Uebungen/Uebungen7/Uebung3/uebung3/src/views/About.vue
@@ -9,85 +9,107 @@
       Ø {{subjectAverage(subject.grades)}}
       </li>
     </ul>
-    <h4>Gesammtdurchschnitt: {{overallAverage}}</h4>
+    <h4>Gesamtdurchschnitt: {{overallAverage}}</h4>
     <h4> Mangelpunkte: {{flaws}}</h4>
+
+    <br>
+    <br>
+    <label for="grade">Note:</label>
+    <input name="grade" v-model="newgrade" />
+
+    <label for="subject"> im Fach:</label>
+    <select name="subject" v-model="corrsubject">
+    <option value="Mathematik">Mathematik</option>
+    <option value="Geschichte">Geschichte</option>
+    <option value="Physik">Physik</option>
+    <option value="Deutsch">Deutsch</option>
+  </select>
+    <button v-on:click="addGrade()">Note hinzufügen</button>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data: function(){
-    return{
+  name: "App",
+  data: function () {
+    return {
       subjects: [
-    {
-        name: "Mathematik",
-        grades: [5,4.7,3.5,4.1] 
-    },
-    {
-        name: "Geschichte",
-        grades: [5.5,4.9,5.75] 
-    },
-    {
-        name: "Physik",
-        grades: [2,2.7,4.3] 
-    },
-    {
-        name: "Deutsch",
-        grades: [4.25,4.1,3.5] 
-    },
-]
-
-    }
+        {
+          name: "Mathematik",
+          grades: [5, 4.7, 3.5, 4.1],
+        },
+        {
+          name: "Geschichte",
+          grades: [5.5, 4.9, 5.75],
+        },
+        {
+          name: "Physik",
+          grades: [2, 2.7, 4.3],
+        },
+        {
+          name: "Deutsch",
+          grades: [4.25, 3.1, 3.5],
+        },
+      ],
+      newgrade: 0,
+      corrsubject: ""
+    };
   },
   methods: {
-    subjectAverage: function(grades){
+    subjectAverage: function (grades) {
       var total = 0;
 
-      grades.forEach(grade => {
+      grades.forEach((grade) => {
         total += grade;
       });
 
-      return Math.round(total / grades.length *2)/2;
+      return Math.round((total / grades.length) * 2) / 2;
+    }, 
+    addGrade: function(){
+      this.subjects.forEach((subject) => {
+        if(subject.name == this.corrsubject){
+          subject.grades.push(parseInt(this.newgrade));
+        }
+      });
+      this.corrsubject = ''
+      this.newgrade = 0;
     }
   },
   computed: {
-    overallAverage: function(){
+    overallAverage: function () {
       var total = 0;
 
-      this.subjects.forEach(subject =>{
+      this.subjects.forEach((subject) => {
         total += parseFloat(this.subjectAverage(subject.grades));
       });
 
-      return total / this.subjects.length;
+      return (total / this.subjects.length).toFixed(2);
     },
-    flaws: function(){
+    flaws: function () {
       var count = 0;
 
-      this.subjects.forEach(subject =>{
+      this.subjects.forEach((subject) => {
         var grade = parseFloat(this.subjectAverage(subject.grades));
 
-        if( grade < 4){
-          count ++;
+        if (grade < 4) {
+          count++;
         }
       });
 
       return count;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 
 <style lang="scss">
-
-.content{
+.content {
   width: 800px;
   margin: 0 auto;
 }
 
-.grades{
+.grades {
   display: inline;
 }
 </style>
